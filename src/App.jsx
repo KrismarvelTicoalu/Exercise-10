6414613371a677071a875b93dd6ce2cffe2b7b1f
chanliearyandousem <aryandochanlie@gmail.com>
2023-10-16 22:26:19 +0800

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  return <h1>App</h1>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+  
 }
 
 export default App
