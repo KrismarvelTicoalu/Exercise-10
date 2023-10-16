61063f96aa52d64dd78aa463dda81a4804cb588e
chanliearyandousem <aryandochanlie@gmail.com>
2023-10-17 07:13:24 +0800

    add Card-Info

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,11 @@
 import Search from "../Search"
+import CardInfo from "../Card-info"
 function Home() {
     return (
       <div>
         <h1>Home</h1>
         <Search/>
+        <CardInfo/>
       </div>
     )
   }
