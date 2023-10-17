f347ccfca42f8b0ca3456172bd7ecea27f718d27
unknown <ndrstimoti@gmail.com>
2023-10-17 21:14:46 +0800

    import evolution, type-effect, dan logo ke header

--- src/components/Card-info/index.jsx
+++ src/components/Card-info/index.jsx
@@ -1,5 +1,13 @@
+import Evolution from "../Evolution"
+import TypeEffect from "../Type-Effect"
+import Logo_CardInfo from "../Logo(Card-Info)"
 function CardInfo() {
-    return <h1>Card-Info</h1>
+    return (
+    <h1>Card-Info</h1>
+    <Evolution/>
+    <TypeEffect/>
+    <Logo_CardInfo/>
+    )
   }
   
   export default CardInfo
