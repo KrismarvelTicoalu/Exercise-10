d4f485738390c5c10ff357d4a6e03c1144bb3309
HizkiaSiregar <s22210002@student.unklab.ac.id>
2023-10-16 21:01:25 -0700

    Add generations & gen 1

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import Logo from "../logo"
+import Generations  from "../generations"
 function Header() {
     return (
     
     <div>
       <h1>Header</h1>
       <Logo/>
+      <Generations/>
     </div>
     
     )
