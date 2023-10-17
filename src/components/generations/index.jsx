7baf85b21de3ecfd51282fbafb5092316630b2b5
HizkiaSiregar <s22210002@student.unklab.ac.id>
2023-10-16 21:21:50 -0700

    Revisi

---
+++ src/components/generations/index.jsx
@@ -0,0 +1,11 @@
+import Gen1 from "../Gen 1/Index"
+function Generations() {
+    return (
+    <div>
+        <h1>Generations</h1>
+        <Gen1 />
+    </div>
+    )
+  }
+  
+  export default Generations
